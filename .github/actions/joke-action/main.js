b7b28a95fa90ca39b50ce3abde4339a03b8f49df
Dan Hardej <danielhardej@github.com>
2023-07-26 16:54:08 +0800

    Update main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -2,20 +2,31 @@
 // we simply use require() to point to the location of the file we wish to bring in.
 const getJoke = require("./joke");
 const core = require('@actions/core');
-const context = require('@actions/github');
+const { context, getOctokit } = require('@actions/github');
 
 // write another asynchronous JavaScript function that stores the return value of getJoke()
 // in a variable called joke.
+// async function run() {
+//   const joke = await getJoke();
+//   // log the joke to the console
+//   console.log(joke);
+//   core.setOutput("joke-output", joke);
+// }
+
 async function run() {
+  const octokit = getOctokit(process.env.GITHUB_TOKEN);
   const joke = await getJoke();
-  // log the joke to the console
   console.log(joke);
   core.setOutput("joke-output", joke);
-  // return the joke as a comment on the issue
-  return context.github.issues.createComment(
-    context.issue({ body: joke })
-  );
-
+  const { owner, repo } = context.repo;
+  const issue_number = context.payload.issue.number;
+  const comment = {
+    owner,
+    repo,
+    issue_number,
+    body: joke
+  };
+  await octokit.rest.issues.createComment(comment);
 }
 
 run();
