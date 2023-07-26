b419dd27e2889234b2de4b1318c00db3ce8f40f4
Dan Hardej <danielhardej@github.com>
2023-07-26 16:15:42 +0800

    update JS files

---
+++ .github/actions/joke-action/main.js
@@ -0,0 +1,15 @@
+// bring in our dependencies - this time, our dependencies include something we wrote! 
+// we simply use require() to point to the location of the file we wish to bring in.
+const getJoke = require("./joke");
+const core = require("@actions/core");
+
+// write another asynchronous JavaScript function that stores the return value of getJoke()
+// in a variable called joke.
+async function run() {
+  const joke = await getJoke();
+  // log the joke to the console
+  console.log(joke);
+  core.setOutput("joke-output", joke);
+}
+
+run();
